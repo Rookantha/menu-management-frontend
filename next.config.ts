6ad570fd7ac7386cecfb65a520259a1428482967
rookantha <rookantha.vc@gmail.com>
2025-02-25 08:28:55 +0530

    port issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",  // Requests to /api/* will be forwarded
+        destination: "https://menu-management.us-east-1.elasticbeanstalk.com/:path*", // Your backend URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
